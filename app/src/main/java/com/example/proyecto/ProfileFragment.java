998bd733356df557dcee0fe240699aa38df9ed02
Shriqui1 <81338119+Shriqui1@users.noreply.github.com>
2021-09-30 17:32:37 -0500

    Camara
    Camara verificacion

--- app/src/main/java/com/example/proyecto/ProfileFragment.java
+++ app/src/main/java/com/example/proyecto/ProfileFragment.java
@@ -1,14 +1,24 @@
 package com.example.proyecto;
 
+import static android.app.Activity.RESULT_OK;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ProfileFragment extends Fragment {
+
+        private Button btnVerificar;
 
     public ProfileFragment(){
 
@@ -18,6 +28,17 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        Button btVerificar = (Button) view.findViewById(R.id.btVerificar);
+        btVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getActivity(), Verificar.class);
+                startActivity(in);
+            }
+        });
+
         return view;
     }
+
 }
+
---
+++ app/src/main/java/com/example/proyecto/Verificar.java
@@ -0,0 +1,94 @@
+package com.example.proyecto;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class Verificar extends AppCompatActivity {
+
+
+    Button buttonine;
+    Button buttoncurp;
+    ImageView imageviewIne;
+    ImageView imageviewCurp;
+    static final int CAM_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verificar);
+
+        buttonine = findViewById(R.id.buttonIne);
+        buttoncurp = findViewById(R.id.buttonCurp);
+        imageviewIne = findViewById(R.id.imageViewIne);
+        imageviewCurp = findViewById(R.id.imageViewCurp);
+
+        if (ContextCompat.checkSelfPermission(Verificar.this, Manifest.permission.CAMERA)
+        != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(Verificar.this, new String[]{
+                    Manifest.permission.CAMERA
+            }, 100);
+
+        }
+
+        buttoncurp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+
+        buttonine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent, CAM_REQUEST);
+
+
+            }
+        });
+
+
+    }
+    private File getFile()
+    {
+        File folder = new File ("sdcard/TiempoCompartido");
+
+        if (!folder.exists())
+        {
+            folder.mkdir();
+        }
+
+        File image_file = new File (folder,"camimage.jpg");
+
+        return image_file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageviewCurp.setImageBitmap(bitmap);
+        }
+    }
+}
