eda7708614a23fabd9ab638671cca49ce6c2cc64
oscarert2 <79671666+oscarert2@users.noreply.github.com>
2021-09-30 17:33:00 -0500

    segundo intento
    firebase aun no funciona

--- app/src/main/java/com/example/proyecto/SignUpActivity.java
+++ app/src/main/java/com/example/proyecto/SignUpActivity.java
@@ -3,17 +3,12 @@ package com.example.proyecto;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,7 +19,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseDatabase Database;
     private DatabaseReference mDatabase;
@@ -34,6 +29,8 @@ public class SignUpActivity extends AppCompatActivity {
     public void aute(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        View usernmView = findViewById(R.id.etCreaUsuario);
+        String usernm = usernmView.toString();
         View emailView = findViewById(R.id.editTextTextEmailAddress);
         String email = emailView.toString();
         View passwordView = findViewById(R.id.etCreaContrasena);
@@ -48,13 +45,25 @@ public class SignUpActivity extends AppCompatActivity {
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                             mAuth = FirebaseAuth.getInstance();
+                            mDatabase.child("users").child("test").child("username").setValue(email);
+                            User user1 = new User(usernm, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(SignUpActivity.this, "registrado", Toast.LENGTH_LONG).show();
+                                    }else{
+                                        Toast.makeText(SignUpActivity.this, "Fail", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
 
                         } else {
                             //If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+                            Toast.makeText(SignUpActivity.this, "Fail", Toast.LENGTH_LONG).show();
                         }
 
                         // ...
@@ -71,5 +80,10 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         Button btnSignUp = (Button) findViewById(R.id.btnSignUp);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
