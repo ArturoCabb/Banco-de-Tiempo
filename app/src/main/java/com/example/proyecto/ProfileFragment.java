3fae9e8d9b793d89d555c73d0c525fc42414faf2
ArturoCabb <arturcabbb@outlook.com>
2021-10-05 15:23:06 -0500

    Login listo y MainActivity recuperra el usuario

--- app/src/main/java/com/example/proyecto/ProfileFragment.java
+++ app/src/main/java/com/example/proyecto/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.proyecto;
 
 import static android.app.Activity.RESULT_OK;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -18,10 +19,35 @@ import android.widget.ImageView;
 
 public class ProfileFragment extends Fragment {
 
-        private Button btnVerificar;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     public ProfileFragment(){
 
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BlankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
@@ -38,6 +64,12 @@ public class ProfileFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 
 }
